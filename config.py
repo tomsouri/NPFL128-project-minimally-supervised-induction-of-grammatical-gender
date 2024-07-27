c0fde3d30a4bb17e2d97e094592a3770b0967fc2
tomas.sourada <tomas.sourada@seznam.cz>
2024-07-27 15:55:55 +0200

    config: path to data files

---
+++ config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).parent
+
+EN_MASC_SEEDS_FILEPATH = PROJECT_DIR / 'data' / 'masc.txt'
+EN_FEM_SEEDS_FILEPATH = PROJECT_DIR / 'data' / 'fem.txt'
