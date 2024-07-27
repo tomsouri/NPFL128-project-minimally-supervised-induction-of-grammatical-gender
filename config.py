897368294006d84e635caa464fd1a0f58be4aa29
tomas.sourada <tomas.sourada@seznam.cz>
2024-07-27 23:16:29 +0200

    config: paths to data files

--- config.py
+++ config.py
@@ -2,5 +2,12 @@ from pathlib import Path
 
 PROJECT_DIR = Path(__file__).parent
 
-EN_MASC_SEEDS_FILEPATH = PROJECT_DIR / 'data' / 'masc.txt'
-EN_FEM_SEEDS_FILEPATH = PROJECT_DIR / 'data' / 'fem.txt'
+DATA_DIR = PROJECT_DIR / 'data'
+
+EN_MASC_SEEDS_FILEPATH = DATA_DIR / 'masc.txt'
+EN_FEM_SEEDS_FILEPATH = DATA_DIR / 'fem.txt'
+
+
+
+
+
