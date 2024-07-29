6db74aba71b72d3e41c53efd8ddf11ee723172d7
tomas.sourada <tomas.sourada@seznam.cz>
2024-07-29 23:53:54 +0200

    seeds as sets

--- seeding/obtain_seeds.py
+++ seeding/obtain_seeds.py
@@ -35,7 +35,7 @@ def filter_phrases(word_list: list[str]) -> list[str]:
     return [word for word in word_list if len(word.split(" ")) == 1]
 
 
-def obtain_seeds(en_masc_seeds_filepath, en_fem_seeds_filepath) -> tuple[list[str], list[str]]:
+def obtain_seeds(en_masc_seeds_filepath, en_fem_seeds_filepath) -> tuple[set[str], set[str]]:
     """
     Returns the list of seed nouns for the Czech language, as a tuple of list of masc and fem seed nouns.
     :return: list of masculine seeds, list of feminine seeds
@@ -51,7 +51,7 @@ def obtain_seeds(en_masc_seeds_filepath, en_fem_seeds_filepath) -> tuple[list[st
 
     collisions = detect_translation_gender_collisions(cz_masc_filtered, cz_fem_filtered)
 
-    cz_masc_no_collisions = [w for w in cz_masc_filtered if w not in collisions]
-    cz_fem_no_collisions = [w for w in cz_fem_filtered if w not in collisions]
+    cz_masc_no_collisions = set([w for w in cz_masc_filtered if w not in collisions])
+    cz_fem_no_collisions = set([w for w in cz_fem_filtered if w not in collisions])
 
-    return (cz_masc_no_collisions, cz_fem_no_collisions)
+    return cz_masc_no_collisions, cz_fem_no_collisions
